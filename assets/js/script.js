c4ec432371078f184e414ed3caa61074b15dada0
Elizeu Junior <131943477+elizeujrdev@users.noreply.github.com>
2023-08-31 11:48:25 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+  }
+}
+
+function streamers() {
+
+  window.location.href = "streamers.html"
+
+}
